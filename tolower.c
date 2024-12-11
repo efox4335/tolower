8e3e70202dc811ab812f9c71c8eb7cf5c1ecb523
Emma Christy <emma.t.christy@gmail.com>
2024-12-11 10:28:33 +0200

    inital commit

---
+++ tolower.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX_LINUX_FILE_NAME_BUFF 5000
+
+int main(void)
+{
+	FILE *fp = NULL;
+	char ls_output[MAX_LINUX_FILE_NAME_BUFF];
+	char lower[MAX_LINUX_FILE_NAME_BUFF];
+	char command[(MAX_LINUX_FILE_NAME_BUFF * 2) + 10];// extra space added to hold mv
+
+	fp = popen("ls -a", "r");
+
+	while(NULL != fgets(ls_output, MAX_LINUX_FILE_NAME_BUFF, fp)){
+		for(int i = 0; ls_output[i] != '\0'; ++i){
+			if(isalpha(ls_output[i])){
+				lower[i] = tolower(ls_output[i]);
+			}else if(ls_output[i] == '\n'){
+				lower[i] = '\0';
+				ls_output[i] = '\0';
+			}else if(ls_output[i] == '\r'){
+				lower[i] = '\0';
+				ls_output[i] = '\0';
+			}else{
+				lower[i] = ls_output[i];
+			}
+		}
+
+		if(strcmp(lower, ls_output)){//avoids mv same dest error
+			sprintf(command, "mv %s %s", ls_output, lower);
+			system(command);
+		}
+	}
+
+	return 0;
+}
